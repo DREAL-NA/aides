060998d4f3e7ac362711831764ea154003ec143e
Nicolas Giraud <contact@ngiraud.me>
2018-01-24 13:27:46 +0100

    Migrations for multiple benef., project holders and perimeters

---
+++ database/migrations/2018_01_24_121900_create_beneficiaries_call_for_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBeneficiariesCallForProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beneficiaries_call_for_projects', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->integer('beneficiary_id')->unsigned()->index();
+	        $table->integer('call_for_project_id')->unsigned()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beneficiaries_call_for_projects');
+    }
+}
---
+++ database/migrations/2018_01_24_122139_create_call_for_projects_perimeters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCallForProjectsPerimetersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('call_for_projects_perimeters', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->integer('perimeter_id')->unsigned()->index();
+	        $table->integer('call_for_project_id')->unsigned()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('call_for_projects_perimeters');
+    }
+}
