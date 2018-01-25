6a77ee174a157f5963f174a266b211fa6f7d63b9
Nicolas Giraud <contact@ngiraud.me>
2018-01-25 13:31:15 +0100

    [FO] Style

--- resources/views/front/_header.blade.php
+++ resources/views/front/_header.blade.php
@@ -1,10 +1,10 @@
 <header>
-	<a href="{{ route('front.home') }}" class="link-marianne">
-		{{--@include('svg.marianne')--}}
-		{{--<span class="title">DREAL <br>Nouvelle-Aquitaine</span>--}}
-		<img src="{{ asset('images/logo_1.png') }}" alt="DREAL Nouvelle-Aquitaine" class="logo-image">
-	</a>
 	<div class="container">
+		<a href="{{ route('front.home') }}" class="link-marianne">
+			{{--@include('svg.marianne')--}}
+			{{--<span class="title">DREAL <br>Nouvelle-Aquitaine</span>--}}
+			<img src="{{ asset('images/logo_1.png') }}" alt="DREAL Nouvelle-Aquitaine" class="logo-image">
+		</a>
 		<a href="{{ route('front.home') }}" class="link-title">
 			<h1>{{ ucfirst(config('app.domain')) }}</h1>
 			<p class="description">La base de données de référence sur les aides aux territoires ouverte à tous</p>
