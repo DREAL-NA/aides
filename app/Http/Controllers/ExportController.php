7d4f986e13e9e3b6ce1d3b6dcaca19a3b8522f73
Nicolas Giraud <contact@ngiraud.me>
2018-02-08 09:36:28 +0100

    Export Excel bug en prod mais pas en dev

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -62,7 +62,7 @@ class ExportController extends Controller
         $this->callsForProjects = $callsForProjects;
 
         $date = date('YmdHis');
-        $this->filename = 'dispo_finan_' . $date;
+        $this->filename = 'dispositifs_financiers_' . $date;
     }
 
     public function xlsx($type)
