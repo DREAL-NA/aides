762a03ba095a436af7235ac0bee9aafea10d1161
Nicolas Giraud <contact@ngiraud.me>
2018-03-12 19:59:09 +0100

    bko sur un container-fluid

--- resources/views/layouts/bko.blade.php
+++ resources/views/layouts/bko.blade.php
@@ -1,110 +1,110 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- CSRF Token -->
-		<meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-		<title>{{ (app()->environment() == 'production' ? '' : 'DEV - ').'BKO - '.config('app.name', 'DREAL') }}</title>
+    <title>{{ (app()->environment() == 'production' ? '' : 'DEV - ').'BKO - '.config('app.name', 'DREAL') }}</title>
 
-		<!-- Styles -->
-		<link href="{{ mix('css/bko.css') }}" rel="stylesheet">
-	</head>
-	<body>
-		<div id="app">
-			<nav class="navbar navbar-default navbar-static-top">
-				<div class="container">
-					<div class="navbar-header">
+    <!-- Styles -->
+    <link href="{{ mix('css/bko.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
 
-						<!-- Collapsed Hamburger -->
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-							<span class="sr-only">Toggle Navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-						<!-- Branding Image -->
-						<a class="navbar-brand" href="{{ url('/') }}">
-							{{ config('app.name', 'Laravel') }}
-						</a>
-					</div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
 
-					<div class="collapse navbar-collapse" id="app-navbar-collapse">
-						<!-- Left Side Of Navbar -->
-						<ul class="nav navbar-nav">
-							&nbsp;
-						</ul>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
 
-						<!-- Right Side Of Navbar -->
-						<ul class="nav navbar-nav navbar-right">
-							<!-- Authentication Links -->
-							@guest
-								<li><a href="{{ route('login') }}">S'authentifier</a></li>
-							@else
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-										{{ Auth::user()->name }} <span class="caret"></span>
-									</a>
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a href="{{ route('login') }}">S'authentifier</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-									<ul class="dropdown-menu" role="menu">
-										<li>
-											<a href="{{ route('logout') }}"
-											   onclick="event.preventDefault();
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
 												 document.getElementById('logout-form').submit();">
-												Se déconnecter
-											</a>
+                                        Se déconnecter
+                                    </a>
 
-											<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-												{{ csrf_field() }}
-											</form>
-										</li>
-									</ul>
-								</li>
-							@endguest
-						</ul>
-					</div>
-				</div>
-			</nav>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-			<div class="container">
-				<div class="row">
-					@auth
-					<div class="col-md-3">
-						@include('bko._menu')
-					</div>
-					@endauth
-					<div class="{{ auth()->check() ? 'col-md-9' : 'col-md-8 col-md-offset-2' }}">
-						<div class="panel panel-default">
-							<div class="panel-heading">@yield('heading')</div>
-							<div class="panel-body">
-								@if($errors->any())
-									<div class="alert alert-danger">
-										@foreach($errors->all() as $error)
-											<p>{{ $error }}</p>
-										@endforeach
-									</div>
-								@endif
-								@if(Session::has('success'))
-									<div class="alert alert-success">
-										{{ session('success') }}
-									</div>
-								@endif
+    <div class="container-fluid">
+        <div class="row">
+            @auth
+                <div class="col-md-3">
+                    @include('bko._menu')
+                </div>
+            @endauth
+            <div class="{{ auth()->check() ? 'col-md-9' : 'col-md-8 col-md-offset-2' }}">
+                <div class="panel panel-default">
+                    <div class="panel-heading">@yield('heading')</div>
+                    <div class="panel-body">
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <p>{{ $error }}</p>
+                                @endforeach
+                            </div>
+                        @endif
+                        @if(Session::has('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
 
-								@yield('content')
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-		@yield('after-content')
+@yield('after-content')
 
-		<script src="{{ mix('js/bko.js') }}"></script>
-		@stack('inline-script')
-	</body>
+<script src="{{ mix('js/bko.js') }}"></script>
+@stack('inline-script')
+</body>
 </html>
