75724ae84f11b54cf083d2ea160b7d3fb4058896
Nicolas Giraud <contact@ngiraud.me>
2018-10-19 17:00:36 +0200

    Not imported NewBkoUser class notification

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\NewBkoUser;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
