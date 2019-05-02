43ee434f25e18b7ab3c231b24dd77b92c251b31f
Nicolas Giraud <contact@ngiraud.me>
2019-05-02 10:14:23 +0200

    Removing DREAL Logo from header and footer

--- resources/views/front/_header.blade.php
+++ resources/views/front/_header.blade.php
@@ -5,7 +5,6 @@
             {{--<span class="title">DREAL <br>Nouvelle-Aquitaine</span>--}}
             <img src="{{ asset('images/logo_ministere.jpg') }}" alt="Ministère de la Transition Écologique et Solidaire" class="logo-image">
             <img src="{{ asset('images/logo_prefet.png') }}" alt="Préfet de la région Nouvelle-Aquitaine" class="logo-image">
-            <img src="{{ asset('images/logo_dreal.jpg') }}" alt="DREAL Nouvelle-Aquitaine" class="logo-image">
         </a>
         <a href="{{ route('front.home') }}" class="link-title">
             <h1>{{ ucfirst(config('app.name')) }}</h1>
